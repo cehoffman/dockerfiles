3c418903218c091f5c66cf9864cd913679ce4a0f
Chris Hoffman <cehoffman@gmail.com>
2016-01-10 01:16:21 -0600

    Allow using ENV set by link in recursor config for forward zones

--- pdns-recursor/entrypoint.sh
+++ pdns-recursor/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -n "$RECURSOR_FORWARD_ZONES" ]; then
+  eval "RECURSOR_FORWARD_ZONES=\"$RECURSOR_FORWARD_ZONES\""
+  export RECURSOR_FORWARD_ZONES
+fi
+
 /usr/local/bin/confd --onetime --backend env
 
 exec pdns_recursor

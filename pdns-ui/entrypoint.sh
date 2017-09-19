711dbf630c1a87aa40290c1125fc2d3d39a05ecb
Chris Hoffman <cehoffman@gmail.com>
2017-09-19 01:11:27 -0500

    Remove cruft from old pdns admin entrypoint

--- pdns-ui/entrypoint.sh
+++ pdns-ui/entrypoint.sh
@@ -4,12 +4,11 @@
 
 cd /opt/local/powerdns-admin
 
+mkdir -p data/upload/avatar
+
 . flask/bin/activate
 
 ./create_db.py
 ./db_upgrade.py
 
-# PowerDNS 4.0 change the url of the API to include /api/v1
-sed "s/'\/server/'\/api\/v1\/server/g" -i app/models.py
-
-gunicorn -w ${PDNS_ADMIN_NUM_PROCESSES-2} -b 0.0.0.0:80 app:app
+exec gunicorn -w ${PDNS_ADMIN_NUM_PROCESSES-2} -b 0.0.0.0:80 app:app
